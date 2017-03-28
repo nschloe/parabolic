b6c389468c4eb397702c1d554ec9ccfb1f3f83b3
Nico Schlömer <nico.schloemer@gmail.com>
2017-03-28 09:46:21 +0200

    generalize interface, add test example

--- parabolic/time_steppers.py
+++ parabolic/time_steppers.py
@@ -7,37 +7,6 @@ Routines for one time-stepping of the general equation
     \\frac{du}{dt} = F(u).
 
 '''
-from dolfin import (
-    TrialFunction, TestFunction, Function, dx, Constant, solve, assemble,
-    DirichletBC, Expression, KrylovSolver
-    )
-import numpy
-import sympy
-
-
-class ParabolicProblem(object):
-    '''
-    Abstract problem class for use with the time steppers.
-    '''
-    def __init__(self):
-        self.dx = dx
-        self.dx_multiplier = 1.0
-        return
-
-    def get_system(self, t):
-        raise NotImplementedError(
-                'Classes derived from ParabolicProblem must '
-                'implement get_system().'
-                )
-
-    def get_bcs(self, t):
-        raise NotImplementedError(
-                'Classes derived from ParabolicProblem must '
-                'implement get_bcs().'
-                )
-
-    def get_preconditioner(self, t):
-        return None
 
 
 class Dummy():
@@ -49,17 +18,12 @@ class Dummy():
 
     def __init__(self, problem):
         self.problem = problem
-        # (u - u0) / dt = 0
-        # u = u0
         return
 
-    def step(self, u0, t, dt, bcs1,
-             tol=1.0e-10,
-             verbose=True
-             ):
-        u1 = Function(self.problem.V)
-        u1.interpolate(u0)
-        return u1
+    def step(self, u0, t, dt):
+        # (u1 - u0) / dt = 0
+        # u1 = u0
+        return u0
 
 
 class ExplicitEuler():
@@ -71,44 +35,14 @@ class ExplicitEuler():
 
     def __init__(self, problem):
         self.problem = problem
-        # (u - u0) / dt = F(t, u0, v)
-        # u = u0 + dt * F(t, u0, v)
-        #
-        u = TrialFunction(problem.V)
-        v = TestFunction(problem.V)
-        self.M = assemble(u * v * problem.dx_multiplier * problem.dx)
         return
 
-    def step(self, u1, u0, t, dt,
-             tol=1.0e-10,
-             maxiter=100,
-             verbose=True
-             ):
-        v = TestFunction(self.problem.V)
-
-        L, b = self.problem.get_system(t)
-        Lu0 = Function(self.problem.V)
-        L.mult(u0.vector(), Lu0.vector())
-        rhs = assemble(u0 * v * self.problem.dx_multiplier * self.problem.dx)
-        rhs.axpy(dt, -Lu0.vector() + b)
-
-        A = self.M
-
-        # Apply boundary conditions.
-        for bc in self.problem.get_bcs(t + dt):
-            bc.apply(A, rhs)
-
-        # Both Jacobi-and AMG-preconditioners are order-optimal for the mass
-        # matrix. Since Jacobi isn't available in FEniCS (anymore?), use AMG.
-        solver = KrylovSolver('cg', 'amg')
-        solver.parameters['relative_tolerance'] = tol
-        solver.parameters['absolute_tolerance'] = 0.0
-        solver.parameters['maximum_iterations'] = maxiter
-        solver.parameters['monitor_convergence'] = verbose
-        solver.set_operator(A)
-
-        solver.solve(u1.vector(), rhs)
-        return
+    def step(self, u0, t, dt):
+        # (u{k+1} - u{k}) / dt = F(u{k}, t)
+        # u{k+1} = u{k} + dt * F(u{k}, t)
+        b = self.problem.eval_alpha_M_beta_F(1.0, dt, u0, t)
+        u1 = self.problem.solve_alpha_M_beta_F(1.0, 0.0, b, t+dt)
+        return u1
 
 
 class ImplicitEuler():
@@ -120,46 +54,14 @@ class ImplicitEuler():
 
     def __init__(self, problem):
         self.problem = problem
-        # (u - u0) / dt = F(t, u, v)
-        # u - dt * lhs(F(t, u, v)) = u0 + dt * rhs(F(t, u, v))
-        #
-        u = TrialFunction(problem.V)
-        v = TestFunction(problem.V)
-        self.M = assemble(u * v * problem.dx_multiplier * problem.dx)
         return
 
-    def step(self, u1, u0, t, dt,
-             tol=1.0e-10,
-             maxiter=1000,
-             verbose=True,
-             krylov='gmres',
-             preconditioner='ilu'
-             ):
-        L, b = self.problem.get_system(t + dt)
-
-        A = self.M + dt * L
-
-        v = TestFunction(self.problem.V)
-        rhs = assemble(u0 * v * self.problem.dx_multiplier * self.problem.dx)
-        rhs.axpy(dt, b)
-
-        # Apply boundary conditions.
-        for bc in self.problem.get_bcs(t + dt):
-            bc.apply(A, rhs)
-
-        solver = KrylovSolver(krylov, preconditioner)
-        solver.parameters['relative_tolerance'] = tol
-        solver.parameters['absolute_tolerance'] = 0.0
-        solver.parameters['maximum_iterations'] = maxiter
-        solver.parameters['monitor_convergence'] = verbose
-        P = self.problem.get_preconditioner(t + dt)
-        if P:
-            solver.set_operators(A, self.M + dt * P)
-        else:
-            solver.set_operator(A)
-
-        solver.solve(u1.vector(), rhs)
-        return
+    def step(self, u0, t, dt):
+        # (u{k+1} - u{k}) / dt = F(u{k+1}, t+dt)
+        # u{k+1} - dt * F(u{k}, t+dt) = u{k}
+        b = self.problem.eval_alpha_M_beta_F(1.0, 0.0, u0, t)
+        u1 = self.problem.solve_alpha_M_beta_F(1.0, -dt, b, t+dt)
+        return u1
 
 
 class Trapezoidal():
@@ -172,226 +74,192 @@ class Trapezoidal():
 
     def __init__(self, problem):
         self.problem = problem
-        #  (u - u0) / dt = 0.5 * F(t,    u0, v)
-        #                + 0.5 * F(t+dt, u,  v)
-        #
-        u = TrialFunction(problem.V)
-        v = TestFunction(problem.V)
-        self.M = assemble(u * v * problem.dx_multiplier * problem.dx)
         return
 
-    def step(self, u1, u0,
-             t, dt,
-             tol=1.0e-10,
-             verbose=True,
-             maxiter=1000,
-             krylov='gmres',
-             preconditioner='ilu'
-             ):
-        v = TestFunction(self.problem.V)
-
-        L0, b0 = self.problem.get_system(t)
-        L1, b1 = self.problem.get_system(t + dt)
-
-        Lu0 = Function(self.problem.V)
-        L0.mult(u0.vector(), Lu0.vector())
-
-        rhs = assemble(u0 * v * self.problem.dx_multiplier * self.problem.dx)
-        rhs.axpy(-dt * 0.5, Lu0.vector())
-        rhs.axpy(+dt * 0.5, b0 + b1)
-
-        A = self.M + 0.5 * dt * L1
-
-        # Apply boundary conditions.
-        for bc in self.problem.get_bcs(t + dt):
-            bc.apply(A, rhs)
-
-        solver = KrylovSolver(krylov, preconditioner)
-        solver.parameters['relative_tolerance'] = tol
-        solver.parameters['absolute_tolerance'] = 0.0
-        solver.parameters['maximum_iterations'] = maxiter
-        solver.parameters['monitor_convergence'] = verbose
-        solver.set_operator(A)
-
-        solver.solve(u1.vector(), rhs)
-        return
+    def step(self, u0, t, dt):
+        # (u{k+1} - u{k}) / dt = 1/2 * (F(u{k+1}, t+dt) + F(u{k}, t))
+        # u{k+1} - dt/2 * F(u{k+1}, t+dt) = u{k} + dt/2 * F(u{k}, t)
+        b = self.problem.eval_alpha_M_beta_F(1.0, 0.5*dt, u0, t)
+        u1 = self.problem.solve_alpha_M_beta_F(1.0, -0.5*dt, b, t+dt)
+        return u1
 
 
-def heun_step(
-        V,
-        F,
-        u0,
-        t, dt,
-        sympy_dirichlet_bcs,
-        tol=1.0e-10,
-        verbose=True
-        ):
-    # Heun & variants.
-    # alpha = 0.5
-    alpha = 2.0 / 3.0
-    # alpha = 1.0
-    c = [0.0, alpha]
-    A = [[0.0,   0.0],
-         [alpha, 0.0]]
-    b = [1.0 - 1.0 / (2 * alpha), 1.0 / (2 * alpha)]
-
-    return runge_kutta_step(
-            A, b, c,
-            V, F, u0, t, dt,
-            sympy_dirichlet_bcs=sympy_dirichlet_bcs,
-            tol=tol,
-            verbose=verbose
-            )
-
-
-def rk4_step(
-        V,
-        F,
-        u0,
-        t, dt,
-        sympy_dirichlet_bcs=[],
-        tol=1.0e-10,
-        verbose=True
-        ):
-    '''Classical RK4.
-    '''
-    c = [0.0, 0.5, 0.5, 1.0]
-    A = [[0.0, 0.0, 0.0, 0.0],
-         [0.5, 0.0, 0.0, 0.0],
-         [0.0, 0.5, 0.0, 0.0],
-         [0.0, 0.0, 1.0, 0.0]]
-    b = [1.0 / 6.0, 1.0 / 3.0, 1.0 / 3.0, 1.0 / 6.0]
-
-    return runge_kutta_step(
-            A, b, c,
-            V, F, u0, t, dt,
-            sympy_dirichlet_bcs=sympy_dirichlet_bcs,
-            tol=tol,
-            verbose=verbose
-            )
-
-
-def rkf_step(
-        V,
-        F,
-        u0,
-        t, dt,
-        sympy_dirichlet_bcs=[],
-        tol=1.0e-10,
-        verbose=True
-        ):
-    '''Runge--Kutta--Fehlberg method.
-    '''
-    c = [0.0, 0.25, 3.0 / 8.0, 12.0 / 13.0, 1.0, 0.5]
-    A = [[0.0,         0.0,         0.0,         0.0,         0.0,    0.0],
-         [0.25,        0.0,         0.0,         0.0,         0.0,    0.0],
-         [3./32,       9./32,       0.0,         0.0,         0.0,    0.0],
-         [1932./2197, -7200./2197,  7296./2197,  0.0,         0.0,    0.0],
-         [439./216,   -8.,          3680./513,  -845./4104,   0.0,    0.0],
-         [-8./27,      2.,         -3544./2565,  1859./4104, -11./40, 0.0]]
-    # b = [25./216, 0.0, 1408./2565, 2197./4104, -1./5,  0.0] # 4th order
-    b = [16./135, 0.0, 6656./12825,  28561./56430, -9./50, 2./55]  # 5th order
-
-    return runge_kutta_step(
-            A, b, c,
-            V, F, u0, t, dt,
-            sympy_dirichlet_bcs=sympy_dirichlet_bcs,
-            tol=tol,
-            verbose=verbose
-            )
-
-
-def runge_kutta_step(
-        A, b, c,
-        V,
-        F,
-        u0,
-        t, dt,
-        sympy_dirichlet_bcs=[],
-        tol=1.0e-10,
-        verbose=True
-        ):
-    # Make sure that the scheme is strictly upper-triangular.
-    s = len(b)
-    A = numpy.array(A)
-    # Can't handle fully implicit methods.
-    assert numpy.all(abs(A[numpy.triu_indices(s)]) < 1.0e-15)
-
-    u = TrialFunction(V)
-    v = TestFunction(V)
-
-    solver_params = {
-            'linear_solver': 'iterative',
-            'symmetric': True,
-            'preconditioner': 'hypre_amg',
-            'krylov_solver': {
-                'relative_tolerance': tol,
-                'absolute_tolerance': 0.0,
-                'maximum_iterations': 100,
-                'monitor_convergence': verbose
-                }
-            }
-
-    # For the boundary values, see
-    #
-    #   Intermediate Boundary Conditions for Runge-Kutta Time Integration of
-    #   Initial-Boundary Value Problems,
-    #   D. Pathria,
-    #   <http://www.math.uh.edu/~hjm/june1995/p00379-p00388.pdf>.
-    #
-    tt = sympy.symbols('t')
-    BCS = []
-    # Get boundary conditions and their derivatives.
-    for k in range(2):
-        BCS.append([])
-        for boundary, expr in sympy_dirichlet_bcs:
-            # Form k-th derivative.
-            DexprDt = sympy.diff(expr, tt, k)
-            # TODO set degree of expression
-            BCS[-1].append(
-                DirichletBC(
-                    V,
-                    Expression(sympy.printing.ccode(DexprDt), t=t + dt),
-                    boundary
-                    )
-                )
-
-    # Use the Constant() syntax to avoid compiling separate expressions for
-    # different values of dt.
-    ddt = Constant(dt)
-
-    # Compute the stage values.
-    k = []
-    for i in range(s):
-        U = u0
-        for j in range(i):
-            U += ddt * A[i][j] * k[j]
-        L = F(t + c[i] * dt, U, v)
-        k.append(Function(V))
-        # Using this bc is somewhat random.
-        # TODO come up with something better here.
-        for g in BCS[1]:
-            g.t = t + c[i] * dt
-        solve(
-            u * v * dx == L, k[i],
-            bcs=BCS[1],
-            solver_parameters=solver_params
-            )
-        # plot(k[-1])
-        # interactive()
-
-    # Put it all together.
-    U = u0
-    for i in range(s):
-        U += b[i] * k[i]
-    theta = Function(V)
-    for g in BCS[0]:
-        g.t = t + dt
-    solve(
-        u * v * dx == (u0 + ddt * U) * v * dx,
-        theta,
-        bcs=BCS[0],
-        solver_parameters=solver_params
-        )
-
-    return theta
+# def heun_step(
+#         V,
+#         F,
+#         u0,
+#         t, dt,
+#         sympy_dirichlet_bcs,
+#         tol=1.0e-10,
+#         verbose=True
+#         ):
+#     # Heun & variants.
+#     # alpha = 0.5
+#     alpha = 2.0 / 3.0
+#     # alpha = 1.0
+#     c = [0.0, alpha]
+#     A = [[0.0,   0.0],
+#          [alpha, 0.0]]
+#     b = [1.0 - 1.0 / (2 * alpha), 1.0 / (2 * alpha)]
+#
+#     return runge_kutta_step(
+#             A, b, c,
+#             V, F, u0, t, dt,
+#             sympy_dirichlet_bcs=sympy_dirichlet_bcs,
+#             tol=tol,
+#             verbose=verbose
+#             )
+#
+#
+# def rk4_step(
+#         V,
+#         F,
+#         u0,
+#         t, dt,
+#         sympy_dirichlet_bcs=[],
+#         tol=1.0e-10,
+#         verbose=True
+#         ):
+#     '''Classical RK4.
+#     '''
+#     c = [0.0, 0.5, 0.5, 1.0]
+#     A = [[0.0, 0.0, 0.0, 0.0],
+#          [0.5, 0.0, 0.0, 0.0],
+#          [0.0, 0.5, 0.0, 0.0],
+#          [0.0, 0.0, 1.0, 0.0]]
+#     b = [1.0 / 6.0, 1.0 / 3.0, 1.0 / 3.0, 1.0 / 6.0]
+#
+#     return runge_kutta_step(
+#             A, b, c,
+#             V, F, u0, t, dt,
+#             sympy_dirichlet_bcs=sympy_dirichlet_bcs,
+#             tol=tol,
+#             verbose=verbose
+#             )
+#
+#
+# def rkf_step(
+#         V,
+#         F,
+#         u0,
+#         t, dt,
+#         sympy_dirichlet_bcs=[],
+#         tol=1.0e-10,
+#         verbose=True
+#         ):
+#     '''Runge--Kutta--Fehlberg method.
+#     '''
+#     c = [0.0, 0.25, 3.0 / 8.0, 12.0 / 13.0, 1.0, 0.5]
+#     A = [[0.0,         0.0,         0.0,         0.0,         0.0,    0.0],
+#          [0.25,        0.0,         0.0,         0.0,         0.0,    0.0],
+#          [3./32,       9./32,       0.0,         0.0,         0.0,    0.0],
+#          [1932./2197, -7200./2197,  7296./2197,  0.0,         0.0,    0.0],
+#          [439./216,   -8.,          3680./513,  -845./4104,   0.0,    0.0],
+#          [-8./27,      2.,         -3544./2565,  1859./4104, -11./40, 0.0]]
+#     # b = [25./216, 0.0, 1408./2565, 2197./4104, -1./5,  0.0] # 4th order
+#     # 5th order
+#     b = [16./135, 0.0, 6656./12825,  28561./56430, -9./50, 2./55]
+#
+#     return runge_kutta_step(
+#             A, b, c,
+#             V, F, u0, t, dt,
+#             sympy_dirichlet_bcs=sympy_dirichlet_bcs,
+#             tol=tol,
+#             verbose=verbose
+#             )
+#
+#
+# def runge_kutta_step(
+#         A, b, c,
+#         V,
+#         F,
+#         u0,
+#         t, dt,
+#         sympy_dirichlet_bcs=[],
+#         tol=1.0e-10,
+#         verbose=True
+#         ):
+#     # Make sure that the scheme is strictly upper-triangular.
+#     s = len(b)
+#     A = numpy.array(A)
+#     # Can't handle fully implicit methods.
+#     assert numpy.all(abs(A[numpy.triu_indices(s)]) < 1.0e-15)
+#
+#     u = TrialFunction(V)
+#     v = TestFunction(V)
+#
+#     solver_params = {
+#             'linear_solver': 'iterative',
+#             'symmetric': True,
+#             'preconditioner': 'hypre_amg',
+#             'krylov_solver': {
+#                 'relative_tolerance': tol,
+#                 'absolute_tolerance': 0.0,
+#                 'maximum_iterations': 100,
+#                 'monitor_convergence': verbose
+#                 }
+#             }
+#
+#     # For the boundary values, see
+#     #
+#     #   Intermediate Boundary Conditions for Runge-Kutta Time Integration of
+#     #   Initial-Boundary Value Problems,
+#     #   D. Pathria,
+#     #   <http://www.math.uh.edu/~hjm/june1995/p00379-p00388.pdf>.
+#     #
+#     tt = sympy.symbols('t')
+#     BCS = []
+#     # Get boundary conditions and their derivatives.
+#     for k in range(2):
+#         BCS.append([])
+#         for boundary, expr in sympy_dirichlet_bcs:
+#             # Form k-th derivative.
+#             DexprDt = sympy.diff(expr, tt, k)
+#             # TODO set degree of expression
+#             BCS[-1].append(
+#                 DirichletBC(
+#                     V,
+#                     Expression(sympy.printing.ccode(DexprDt), t=t + dt),
+#                     boundary
+#                     )
+#                 )
+#
+#     # Use the Constant() syntax to avoid compiling separate expressions for
+#     # different values of dt.
+#     ddt = Constant(dt)
+#
+#     # Compute the stage values.
+#     k = []
+#     for i in range(s):
+#         U = u0
+#         for j in range(i):
+#             U += ddt * A[i][j] * k[j]
+#         L = F(t + c[i] * dt, U, v)
+#         k.append(Function(V))
+#         # Using this bc is somewhat random.
+#         # TODO come up with something better here.
+#         for g in BCS[1]:
+#             g.t = t + c[i] * dt
+#         solve(
+#             u * v * dx == L, k[i],
+#             bcs=BCS[1],
+#             solver_parameters=solver_params
+#             )
+#         # plot(k[-1])
+#         # interactive()
+#
+#     # Put it all together.
+#     U = u0
+#     for i in range(s):
+#         U += b[i] * k[i]
+#     theta = Function(V)
+#     for g in BCS[0]:
+#         g.t = t + dt
+#     solve(
+#         u * v * dx == (u0 + ddt * U) * v * dx,
+#         theta,
+#         bcs=BCS[0],
+#         solver_parameters=solver_params
+#         )
+#
+#     return theta
---
+++ tests/test_examples.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+#
+import parabolic
+
+from dolfin import (
+    FunctionSpace, DirichletBC, Function, grad, dx, dot, UnitSquareMesh,
+    TrialFunction, TestFunction, assemble, Constant, XDMFFile, KrylovSolver,
+    solve
+    )
+
+
+def test_heat_equation_fenics():
+    # Define problem
+    class Heat(object):
+        '''
+        u' = \Delta u + f
+        '''
+        def __init__(self, V):
+            self.V = V
+            u = TrialFunction(V)
+            v = TestFunction(V)
+            self.M = assemble(u * v * dx)
+            self.A = assemble(-dot(grad(u), grad(v)) * dx)
+            self.f = assemble(1.0 * v * dx)
+            self.bcs = DirichletBC(self.V, 0.0, 'on_boundary')
+            return
+
+        def eval_alpha_M_beta_F(self, alpha, beta, u, t):
+            # Evaluate  alpha * M * u + beta * F(u, t).
+            uvec = u.vector()
+            return alpha * (self.M * uvec) + beta * (self.A * uvec + self.f)
+
+        def solve_alpha_M_beta_F(self, alpha, beta, b, t):
+            # Solve  alpha * M * u + beta * F(u, t) = b for u.
+            A = alpha * self.M + beta * self.A
+            rhs = b - beta * self.f
+            self.bcs.apply(A, rhs)
+
+            solver = KrylovSolver('gmres', 'ilu')
+            solver.parameters['relative_tolerance'] = 1.0e-13
+            solver.parameters['absolute_tolerance'] = 0.0
+            solver.parameters['maximum_iterations'] = 100
+            solver.parameters['monitor_convergence'] = True
+            solver.set_operator(A)
+
+            u = Function(self.V)
+            solver.solve(u.vector(), rhs)
+            return u
+
+    # create initial guess
+    mesh = UnitSquareMesh(20, 20, 'crossed')
+    V = FunctionSpace(mesh, 'CG', 1)
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    u0 = Function(V)
+    solve(u*v*dx == Constant(0.0)*v*dx, u0)
+
+    u1 = Function(V)
+    u1 = u0.copy()
+
+    xf = XDMFFile('heat%04d.xdmf' % 0)
+    xf.write(u1)
+
+    # create time stepper
+    # stepper = parabolic.Dummy(Heat(V))
+    stepper = parabolic.ExplicitEuler(Heat(V))
+    # stepper = parabolic.ImplicitEuler(Heat(V))
+    # stepper = parabolic.Trapezoidal(Heat(V))
+
+    # step
+    t = 0.0
+    dt = 1.0e-3
+    for k in range(10):
+        u1.assign(stepper.step(u0, t, dt))
+        u0 = u1.copy()
+
+        xf = XDMFFile('heat%04d.xdmf' % (k+1))
+        xf.write(u1)
+    return
+
+
+if __name__ == '__main__':
+    test_heat_equation_fenics()
